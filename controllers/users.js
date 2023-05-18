d3081aea6b0261f9cd8e0ecf65dca21d8283765b
kelseyberta <ber20002@byui.edu>
2023-05-18 17:18:56 -0400

    set up all controllers

---
+++ controllers/users.js
@@ -0,0 +1,14 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const updateUser = async (req, res) => {
+    
+  };
+  
+
+//Delete Working
+const deleteUser = async (req, res) => {
+  
+};
+
+module.exports = { updateUser, deleteUser };
