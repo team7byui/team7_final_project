bd2d8e6ca37044a331809e920ca3de46c830ee56
kelseyberta <ber20002@byui.edu>
2023-05-18 17:14:37 -0400

    administration put delete

---
+++ controllers/administration.js
@@ -0,0 +1,41 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+
+const updateAdministration = async (req, res) => {
+    const administrationId = new ObjectId(req.params.id);
+    const administration = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        address: req.body.address,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        position: req.body.birthday
+      };
+    const response = await mongodb
+      .getDb()
+      .db('ClubOrganization')
+      .collection('administration')
+      .replaceOne({ _id: administrationId }, administration);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the admin.');
+    }
+  };
+  
+
+//Delete Working
+const deleteAdministration = async (req, res) => {
+  const administrationId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db('ClubOrganization').collection('administration').deleteOne({ _id: administrationId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Error occurred while deleting the admin.');
+  }
+};
+
+module.exports = { updateAdministration, deleteAdministration };
