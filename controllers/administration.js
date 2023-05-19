ea4c3ab9a08a2314fe87becc2fb2fe1b508c055b
John Sudds <jsuddsjr@users.noreply.github.com>
2023-05-18 23:24:47 -0700

    Add validator chains

--- controllers/administration.js
+++ controllers/administration.js
@@ -1,13 +1,32 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
+const findAll = async (request, response, next) => {
+  try {
+    const result = await mongodb
+      .getDb()
+      .db("ClubOrganization")
+      .collection("administration")
+      .find();
+    result.toArray().then((lists) => {
+      response.setHeader("Content-Type", "application/json");
+      response.status(200).json(lists);
+    });
+  } catch (err) {
+    response.status(500).json(err);
+  }
+};
 
 const findByPosition = async (request, response) => {
   try {
-    const position = new ObjectId(request.params.position);
-    const result = await mongodb.getDb().db('ClubOrganization').collection('administration').find({ _id: position });
+    const position = request.params.position;
+    const result = await mongodb
+      .getDb()
+      .db("ClubOrganization")
+      .collection("administration")
+      .find({ position });
     result.toArray().then((lists) => {
-      response.setHeader('Content-Type', 'application/json');
+      response.setHeader("Content-Type", "application/json");
       response.status(200).json(lists[0]);
     });
   } catch (err) {
@@ -15,7 +34,7 @@ const findByPosition = async (request, response) => {
   }
 };
 
-const createAdministration= async (request, response) => {
+const createAdministration = async (request, response) => {
   try {
     const Administration = {
       firstName: request.body.firstName,
@@ -23,13 +42,21 @@ const createAdministration= async (request, response) => {
       address: request.body.address,
       email: request.body.email,
       phoneNumber: request.body.phoneNumber,
-      position: request.body.position
+      position: request.body.position,
     };
-    const res = await mongodb.getDb().db('ClubOrganization').collection('administration').insertOne(Administration);
+    const res = await mongodb
+      .getDb()
+      .db("ClubOrganization")
+      .collection("administration")
+      .insertOne(Administration);
     if (res.acknowledged) {
       response.status(201).json(res);
     } else {
-      response.status(500).json(res.error || 'Error occurred while creating your administration.');
+      response
+        .status(500)
+        .json(
+          res.error || "Error occurred while creating your administration."
+        );
     }
   } catch (err) {
     response.status(500).json(err);
@@ -37,39 +64,52 @@ const createAdministration= async (request, response) => {
 };
 
 const updateAdministration = async (req, res) => {
-    const administrationId = new ObjectId(req.params.id);
-    const administration = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        address: req.body.address,
-        email: req.body.email,
-        phoneNumber: req.body.phoneNumber,
-        position: req.body.birthday
-      };
-    const response = await mongodb
-      .getDb()
-      .db('ClubOrganization')
-      .collection('administration')
-      .replaceOne({ _id: administrationId }, administration);
-    console.log(response);
-    if (response.modifiedCount > 0) {
-      res.status(204).send();
-    } else {
-      res.status(500).json(response.error || 'Some error occurred while updating the admin.');
-    }
+  const administrationId = new ObjectId(req.params.id);
+  const administration = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    address: req.body.address,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    position: req.body.birthday,
   };
-  
+  const response = await mongodb
+    .getDb()
+    .db("ClubOrganization")
+    .collection("administration")
+    .replaceOne({ _id: administrationId }, administration);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating the admin.");
+  }
+};
 
-//Delete Working
+// Delete Working
 const deleteAdministration = async (req, res) => {
   const administrationId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('ClubOrganization').collection('administration').deleteOne({ _id: administrationId }, true);
+  const response = await mongodb
+    .getDb()
+    .db("ClubOrganization")
+    .collection("administration")
+    .deleteOne({ _id: administrationId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Error occurred while deleting the admin.');
+    res
+      .status(500)
+      .json(response.error || "Error occurred while deleting the admin.");
   }
 };
 
-module.exports = {findByPosition, createAdministration, updateAdministration, deleteAdministration };
+module.exports = {
+  findAll,
+  findByPosition,
+  createAdministration,
+  updateAdministration,
+  deleteAdministration,
+};
--- routes/administration.js
+++ routes/administration.js
@@ -1,10 +1,35 @@
-const routes = require('express').Router();
+const routes = require("express").Router();
+const administrationController = require("../controllers/administration");
+const {
+  idParamRequired,
+  personValidationRules,
+  reportValidationErrors,
+} = require("../middleware/validate");
 
-const administrationController = require('../controllers/administration');
+routes.get("/", administrationController.findAll);
 
-routes.get('/:position', administrationController.findByPosition);
-routes.post('/', administrationController.createAdministration);
-routes.put('/:administrationId', administrationController.updateAdministration);
-routes.delete('/:administrationId', administrationController.deleteAdministration);
+routes.get("/:position", administrationController.findByPosition);
+
+routes.post(
+  "/",
+  personValidationRules(),
+  reportValidationErrors,
+  administrationController.createAdministration
+);
+
+routes.put(
+  "/:administrationId",
+  personValidationRules(),
+  idParamRequired("administrationId"),
+  reportValidationErrors,
+  administrationController.updateAdministration
+);
+
+routes.delete(
+  "/:administrationId",
+  idParamRequired("administrationId"),
+  reportValidationErrors,
+  administrationController.deleteAdministration
+);
 
 module.exports = routes;
